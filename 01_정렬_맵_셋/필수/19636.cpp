453499b12370114f8a8e082eaaf1f78286828b11
Gammja17 <haker_40@naver.com>
2025-02-21 15:37:40 +0900

    [정렬_맵_셋] 2월 21일 - Update
    19636번 수정

--- 01_정렬_맵_셋/필수/19636.cpp
+++ 01_정렬_맵_셋/필수/19636.cpp
@@ -9,30 +9,20 @@ struct Desi {
 };
 
 
-void calWeight1(struct Desi& d) {
-	double change = 0;
-	bool die = false;
-	for (int i = 1; i <= d.D; i++) {
-		change = d.l - (d.l0 + d.A);
-		d.w += change;
-		if (d.w <= 0 || d.l0 <= 0)
-			die = true;
-	}
-	d.afterW = d.w;
-	d.die = die;
-}
-
-void calWeight2(struct Desi& d) {
-	double change, j = 0;//j´Â Àý´ñ°ª ±¸ÇÏ·Á°í µÒ
+void calWeight(struct Desi& d, bool ischange) {
+	double change = 0, j = 0;//j´Â Àý´ñ°ª ±¸ÇÏ·Á°í µÒ
 	bool die = false;
 	d.afterL = d.l0;
 	for (int i = 1; i <= d.D; i++) {
 		change = d.l - (d.afterL + d.A);
 		d.w += change;
-		if (change < 0)
-			j = change * (-1);
-		if (j > d.T) {
-			d.afterL += change / 2;
+		if (ischange)
+		{
+			if (change < 0)
+				j = change * (-1);
+			if (j > d.T) {
+				d.afterL += change / 2;
+			}
 		}
 		if (d.w <= 0 || d.afterL <= 0)
 			die = true;
@@ -47,11 +37,11 @@ int main() {
 	cin >> D1.w >> D1.l0 >> D1.T >> D1.D >> D1.l >> D1.A;
 	D2 = D1;
 
-	calWeight1(D1);
-	calWeight2(D2);
+	calWeight(D1, false);
+	calWeight(D2, true);
 
-	if(D1.die) cout<< "Danger Diet";
-	else cout << D1.afterW << " " << D1.l0<<"\n";
+	if (D1.die) cout << "Danger Diet\n";
+	else cout << D1.afterW << " " << D1.l0 << "\n";
 
 	if (D2.die)
 		cout << "Danger Diet";
