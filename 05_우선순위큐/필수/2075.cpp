961de2c2a3e7db69e90b48e0fd46b23e72ec4925
Gammja17 <haker_40@naver.com>
2025-03-30 23:38:22 +0900

    [우선순위큐]3월 30일 - Update
    2075번 p1 수정

--- 05_우선순위큐/필수/2075.cpp
+++ 05_우선순위큐/필수/2075.cpp
@@ -7,11 +7,6 @@
 
 using namespace std;
 
-struct cmp { // 가장 작은 수가 top이 되도록 바꿈
-	bool operator()(const int& child, const int& parent) {
-		return parent < child;
-	}
-};
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -20,28 +15,24 @@ int main() {
 	int n;
 	cin >> n;
 
-	vector<int> input(n * n); //입력받은 표를 저장할 배열
-	priority_queue<int, vector<int>, cmp> pq; //우선순위큐
+	int num;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	//greater<int> 사용해서 최소힙 만듦
 
 	for (int k = 0; k < n * n; k++) {
-		cin >> input[k];
-		//배열에 역순으로 저장함... 이렇게 하고 중간에 조건을 넣으면 표 전부를 검증할 필요 없이
-		//좀더 효율적으로 할 수 있을것같은데
-		//그건 아직 생각중이라 일단 역순으로 넣었음...
-
-	}
-
-	for (int i = n * n - 1; i >= 0; i--) {
-		if (pq.size() >= n) { //큐의 크기가 n이면 수를 교체할지 말지 체크
-			if (pq.top() < input[i]) //현재 top보다 input이 더 크면
+		cin >> num;
+		
+		if (pq.size() >= n) {//큐의 크기가 n이면 수를 교체할지 말지 체크
+			if (pq.top() < num) //현재 top보다 num이 더 크면
 				//가장 작은 수를 교체
 			{
 				pq.pop();
-				pq.push(input[i]);
+				pq.push(num);
 			}
 		}
-		else pq.push(input[i]); //큐의 크기가 n보다 작을 땐 그냥 저장
+		else pq.push(num); //큐의 크기가 n보다 작을 땐 그냥 저장
 	}
+
 
 	cout << pq.top();
 
